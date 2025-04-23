d4a6dbba224c384fd547e746a9ea6993391e97e2
Grace Rodrigues <grodrigues1@csustan.edu>
2025-04-23 09:16:36 -0700

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -83,20 +83,11 @@ const CONFIG = {
       'https://github.com/grodrigues-1/grodrigues-1.github.io/raw/main/DemoCV.pdf', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
-    'PHP',
-    'Laravel',
-    'JavaScript',
-    'React.js',
-    'Node.js',
-    'Nest.js',
-    'MySQL',
-    'PostgreSQL',
-    'Git',
-    'Docker',
-    'PHPUnit',
-    'CSS',
-    'Antd',
-    'Tailwind',
+    'Bioinformatics',
+    'Microbiology',
+    'Chemistry',
+    'First Aid',
+    'CPR',
   ],
   experiences: [
     {
@@ -124,16 +115,10 @@ const CONFIG = {
   ],
   educations: [
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2015',
-      to: '2019',
-    },
-    {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2012',
-      to: '2014',
+      institution: 'California State University, Stanislaus',
+      degree: 'BS in Biological Sciences',
+      from: '2021',
+      to: '2026',
     },
   ],
   publications: [
