0cd14b60376ef4512578f30918d408de704c73ff
Grace Rodrigues <grodrigues1@csustan.edu>
2025-04-28 10:22:00 -0700

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -33,20 +33,20 @@ const CONFIG = {
       // To hide the `External Projects` section, keep it empty.
       projects: [
         {
-          title: 'Project Name',
+          title: 'Ecology Presentation',
           description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+            'Saving the Monarch Butterflies' Abies Religiosas',
           imageUrl:
             'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
+          link: 'file:///Users/gracemrodrigues/Downloads/Ecology%20Presentation.pdf',
         },
         {
-          title: 'Project Name',
+          title: 'Organic Chemistry Presentation',
           description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+            'Discovering Unknown Dipeptides',
           imageUrl:
             'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
+          link: 'file:///Users/gracemrodrigues/Downloads/Ochem%202%20pres.pptx.pdf',
         },
       ],
     },
